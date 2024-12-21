a2b9e8efd540af2119d6f1d191a6f3b9bc8d0415
H1ggsK <theohavel@proton.me>
2024-12-20 20:39:25 -0800

    Added 1.21.1 version

---
+++ src/main/java/com/h1ggsk/baqdupe/Baqdupe.java
@@ -0,0 +1,10 @@
+package com.h1ggsk.baqdupe;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Baqdupe implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
